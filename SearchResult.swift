2b3bad98648535cc81cad9eac357310b48380c9a
SAVdev888 <f2711sav@gmail.com>
2016-07-05 22:45:44 +0300

    Add files via upload

---
+++ SearchResult.swift
@@ -0,0 +1,16 @@
+//
+//  SearchResult.swift
+//  NewsList
+//
+//  Created by air888 on 01.07.16.
+//  Copyright © 2016 AntonSvita. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class News {
+    
+    var title = ""
+    
+}
